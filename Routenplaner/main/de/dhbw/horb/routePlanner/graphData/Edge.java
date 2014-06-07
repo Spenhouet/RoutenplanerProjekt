b7987e45716f32b5d59dff9f62c336f5b78a62cd
Spenhouet <s.penhouet@outlook.de>
2014-06-08 00:47:42 +0200

    Attribute, Konstruktor und getter / setter zu Node und Edge hinzugefügt.

--- Routenplaner/main/de/dhbw/horb/routePlanner/graphData/Edge.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/graphData/Edge.java
@@ -2,4 +2,52 @@ package de.dhbw.horb.routePlanner.graphData;
 
 public class Edge {
 
+	private String source;
+	private String target;
+	private int number;
+	private int weight;
+	
+	
+	public Edge(String source, String target, int number, int weight){
+		setSource(source);
+		setTarget(target);
+		setNumber(number);
+		setWeight(weight);
+		
+	}
+	
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public String getTarget() {
+		return target;
+	}
+	
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	
 }
