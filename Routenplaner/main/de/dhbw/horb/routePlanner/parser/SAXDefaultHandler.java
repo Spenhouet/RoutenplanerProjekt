c2ba7a665332af041e95104885dd301b12f6b898
Spenhouet <s.penhouet@outlook.de>
2014-06-08 01:07:04 +0200

    Übersichtlicher gemacht

--- Routenplaner/main/de/dhbw/horb/routePlanner/parser/SAXDefaultHandler.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/parser/SAXDefaultHandler.java
@@ -4,23 +4,17 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class SAXDefaultHandler extends DefaultHandler 
-{
+public class SAXDefaultHandler extends DefaultHandler {
 	public  String[]     args;
 	public  StringBuffer sbResult = new StringBuffer();
 	private int          iState   = 0;
 
 	@Override
-	public void startElement( String namespaceURI,
-	                          String localName,
-	                          String qName,
-	                          Attributes attrs )
-	throws SAXException
-	{
+	public void startElement(String namespaceURI, String localName, String qName, Attributes attrs)	throws SAXException	{
 		if( 5 <= iState )  return;
 	    String eName = ( "".equals( localName ) ) ? qName : localName;
-	    if( null != eName )
-	    {
+	    
+	    if( null != eName ) {
 	    	if( eName.equals( args[1] ) )                 // <ParentElem>
 	    		iState = 1;
 	    	if( 1 == iState && eName.equals( args[2] ) )  // <ChildElem>
@@ -33,16 +27,13 @@ public class SAXDefaultHandler extends DefaultHandler
 	}
 
 	@Override
-	public void endElement( String namespaceURI,
-	                        String localName,
-	                        String qName )
-	throws SAXException
-	{
-		if( 5 <= iState )  return;
+	public void endElement(String namespaceURI, String localName, String qName)	throws SAXException	{
 		
-	    String eName = ( "".equals( localName ) ) ? qName : localName;
-	    if( null != eName )
-	    {
+		if(5 <= iState)  return;
+		
+	    String eName = ("".equals( localName) ) ? qName : localName;
+	    
+	    if(null != eName) {
 	    	if( eName.equals( args[1] ) )                 // <ParentElem>
 	    		iState = 0;
 	    	if( 2 == iState && eName.equals( args[2] ) )  // <ChildElem>
@@ -55,9 +46,7 @@ public class SAXDefaultHandler extends DefaultHandler
 	}
 
 	@Override
-	public void characters( char[] buf, int offset, int len )
-	throws SAXException
-	{
+	public void characters( char[] buf, int offset, int len ) throws SAXException {
 		if( 5 <= iState )  return;
 	    
 		String s = new String( buf, offset, len );
