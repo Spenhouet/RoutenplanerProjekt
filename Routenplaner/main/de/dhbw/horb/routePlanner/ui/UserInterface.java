fc02e36b09ccd6587253ae3885cc718156754811
Spenhouet <s.penhouet@outlook.de>
2014-06-08 00:21:50 +0200

    added sax parser classes and some code from the internet

--- Routenplaner/main/de/dhbw/horb/routePlanner/ui/UserInterface.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/ui/UserInterface.java
@@ -1,10 +1,24 @@
 package de.dhbw.horb.routePlanner.ui;
 
+import de.dhbw.horb.routePlanner.parser.SAXGraphParser;
+
 public class UserInterface {
 
 	public static void main(String[] args) {
 		// TODO Automatisch generierter Methodenstub
-
+		
+		SAXGraphParser parser = new SAXGraphParser();
+		
+		String[] args1 = new String[5];
+		
+		args1[0] = "file/graph_bab.xml";
+		args1[1] = "Node";
+		args1[2] = "Title";
+		args1[3] = "Mein zweiter Button";
+		args1[4] = "Comment";
+		
+		parser.main(args1);
+		
 	}
 
 }
