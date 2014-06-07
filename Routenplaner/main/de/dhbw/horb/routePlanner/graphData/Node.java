2f6441a7a633bf64e3293d17e7ed752238273c0a
Spenhouet <s.penhouet@outlook.de>
2014-06-08 00:47:42 +0200

    Attribute, Konstruktor und getter / setter zu Node und Edge hinzugefügt.

--- Routenplaner/main/de/dhbw/horb/routePlanner/graphData/Node.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/graphData/Node.java
@@ -2,4 +2,44 @@ package de.dhbw.horb.routePlanner.graphData;
 
 public class Node {
 
+	private String name;
+	private float latitude;
+	private float longitude;
+	
+	
+	public Node(String name, float latitude, float longitude) {
+		
+		setName(name);
+		setLatitude(latitude);
+		setLongitude(longitude);
+	}
+	
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	
+
+	
 }
