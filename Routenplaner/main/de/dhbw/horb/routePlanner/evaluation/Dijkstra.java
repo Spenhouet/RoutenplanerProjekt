30aadca2f11994d9b662c9d27af7d073ecd38358
Spenhouet <s.penhouet@outlook.de>
2014-06-09 11:53:25 +0200

    Klasse für Grafische Oberfläche, Dijkstra Algo und eine Route hinzugefügt.

---
+++ Routenplaner/main/de/dhbw/horb/routePlanner/evaluation/Dijkstra.java
@@ -0,0 +1,5 @@
+package de.dhbw.horb.routePlanner.evaluation;
+
+public class Dijkstra {
+
+}
---
+++ Routenplaner/main/de/dhbw/horb/routePlanner/ui/GraphicalUserInterface.java
@@ -0,0 +1,25 @@
+package de.dhbw.horb.routePlanner.ui;
+
+import de.dhbw.horb.routePlanner.graphData.Edge;
+import de.dhbw.horb.routePlanner.graphData.Node;
+
+public class GraphicalUserInterface {
+	
+	public GraphicalUserInterface(){
+		
+		
+	}
+	
+	
+	public void addNode(Node newNode){
+		
+		
+		
+	}
+	
+	public void addEdge(Edge newEdge){
+		
+	}
+	
+	
+}
