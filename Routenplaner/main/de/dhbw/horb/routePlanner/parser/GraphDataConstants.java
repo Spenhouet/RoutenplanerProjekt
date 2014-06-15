cfc4b58070c0f3a0e9b56d36ac976e8f03901795
Spenhouet <s.penhouet@outlook.de>
2014-06-15 18:13:42 +0200

    Vorbereiten für neuen Branch mit neuen Daten

--- Routenplaner/main/de/dhbw/horb/routePlanner/parser/GraphDataConstants.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/parser/GraphDataConstants.java
@@ -2,7 +2,7 @@ package de.dhbw.horb.routePlanner.parser;
 
 public class GraphDataConstants {
 
-	public final static String CONST_XML_GRAPH_FILE = "file/graph_bab.xml";
+	public final static String CONST_XML_GRAPH_FILE = "file/graphData.xml";
 	
 	public final static String CONST_NODE = "node";
 	public final static String CONST_NODE_ID = "id";
