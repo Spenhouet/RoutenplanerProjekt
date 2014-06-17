bab9e95bbd0a3607a79e4ee8c35c66226f9ea903
Spenhouet <s.penhouet@outlook.de>
2014-06-17 16:26:02 +0200

    XMLStreamReader extends

--- Routenplaner/main/de/dhbw/horb/routePlanner/graphData/Way.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/graphData/Way.java
@@ -14,20 +14,8 @@ public class Way {
 
 	// private List<String, Object> characteristics;
 
-	/**
-	 * Konstruktor der Kanten Klasse.
-	 * 
-	 * @param source
-	 *            Der Anfang einer Kante.
-	 * @param target
-	 *            Das Ende einer Kante.
-	 * @param identifier
-	 *            Eine Straﬂenbezeichnung.
-	 * @param length
-	 *            Die L‰nge der Kante.
-	 */
 	public Way(Long id, List<Long> nodeIDs, List<String> characteristics) {
-
+		
 	}
 
 }
--- Routenplaner/main/de/dhbw/horb/routePlanner/parser/GraphDataParser.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/parser/GraphDataParser.java
@@ -16,7 +16,7 @@ import de.dhbw.horb.routePlanner.ui.GraphicalUserInterface;
  * 
  * @author Sebastian
  */
-public class GraphDataParser implements GraphDataConstants {
+public class GraphDataParser {
 
 	// TODO bestimmten Knoten und Kante suchen
 
@@ -36,17 +36,19 @@ public class GraphDataParser implements GraphDataConstants {
 		}
 	}
 
-	private XMLStreamReader streamReader;
+	private GraphDataStreamReader graphSR;
 
 	private GraphDataParser(String xmlFile) throws FileNotFoundException,
 			XMLStreamException {
 		XMLInputFactory factory = XMLInputFactory.newInstance();
-		streamReader = factory.createXMLStreamReader(new FileReader(xmlFile));
+		graphSR = (GraphDataStreamReader) factory
+				.createXMLStreamReader(new FileReader(xmlFile));
+
 	}
 
 	public void everyNodeToGui(GraphicalUserInterface gui)
 			throws XMLStreamException {
-		while (streamReader.hasNext()) {
+		while (graphSR.hasNext()) {
 			if (nextStartElement()) {
 				Node nextNode = getNode(null);
 				if (nextNode != null)
@@ -57,12 +59,12 @@ public class GraphDataParser implements GraphDataConstants {
 
 	public void everyWayToGui(GraphicalUserInterface gui)
 			throws XMLStreamException {
-		while (streamReader.hasNext()) {
-			streamReader.next();
+		while (graphSR.hasNext()) {
+			graphSR.next();
 			if (nextStartElement()) {
 				Way nextWay = getWay(null);
-//				if (nextWay != null)
-//					gui.addWay(nextWay);
+				// if (nextWay != null)
+				// gui.addWay(nextWay);
 			}
 		}
 	}
@@ -73,24 +75,28 @@ public class GraphDataParser implements GraphDataConstants {
 		Double lon = null;
 
 		if (id == null) {
-			if (streamReader.getLocalName() != CONST_NODE)
+			if (graphSR.getLocalName() != GraphDataConstants.CONST_NODE)
 				return null;
-			id = Long.valueOf(getAttributeValue(CONST_NODE_ID));
-			lat = Double.valueOf(getAttributeValue(CONST_NODE_LATITUDE));
-			lon = Double.valueOf(getAttributeValue(CONST_NODE_LONGITUDE));
+			id = Long
+					.valueOf(getAttributeValue(GraphDataConstants.CONST_NODE_ID));
+			lat = Double
+					.valueOf(getAttributeValue(GraphDataConstants.CONST_NODE_LATITUDE));
+			lon = Double
+					.valueOf(getAttributeValue(GraphDataConstants.CONST_NODE_LONGITUDE));
 		} else {
-			while (streamReader.hasNext()) {
-				if (streamReader.getLocalName() == CONST_NODE
-						&& id == Long.valueOf(getAttributeValue(CONST_NODE_ID))) {
+			while (graphSR.hasNext()) {
+				if (graphSR.getLocalName() == GraphDataConstants.CONST_NODE
+						&& id == Long
+								.valueOf(getAttributeValue(GraphDataConstants.CONST_NODE_ID))) {
 					lat = Double
-							.valueOf(getAttributeValue(CONST_NODE_LATITUDE));
+							.valueOf(getAttributeValue(GraphDataConstants.CONST_NODE_LATITUDE));
 					lon = Double
-							.valueOf(getAttributeValue(CONST_NODE_LONGITUDE));
+							.valueOf(getAttributeValue(GraphDataConstants.CONST_NODE_LONGITUDE));
 					break;
 				}
 			}
 		}
-		streamReader.close();
+		graphSR.close();
 		if (id != null && lat != null && lon != null)
 			return (new Node(id, lat, lon));
 
@@ -100,9 +106,10 @@ public class GraphDataParser implements GraphDataConstants {
 	private Way getWay(Long id) throws NumberFormatException,
 			XMLStreamException {
 		if (id == null) {
-			if (streamReader.getLocalName() != CONST_WAY)
+			if (graphSR.getLocalName() != GraphDataConstants.CONST_WAY)
 				return null;
-			id = Long.valueOf(getAttributeValue(CONST_WAY_ID));
+			id = Long
+					.valueOf(getAttributeValue(GraphDataConstants.CONST_WAY_ID));
 
 		} else {
 
@@ -115,15 +122,15 @@ public class GraphDataParser implements GraphDataConstants {
 		// getNextCharacter(CONST_WAY_NODE, CONST_WAY_REF
 		//
 		// if(id != null) return (new Way(id));
-		streamReader.close();
+		graphSR.close();
 		return null;
 	}
 
 	private String getAttributeValue(String AttributeLocalName) {
-		for (int x = 0; x < streamReader.getAttributeCount(); x++)
-			if (streamReader.getAttributeLocalName(x).trim()
+		for (int x = 0; x < graphSR.getAttributeCount(); x++)
+			if (graphSR.getAttributeLocalName(x).trim()
 					.equals(AttributeLocalName))
-				return streamReader.getAttributeValue(x);
+				return graphSR.getAttributeValue(x);
 
 		return null;
 	}
@@ -132,18 +139,18 @@ public class GraphDataParser implements GraphDataConstants {
 			String attributeLocalName, String attributeValue)
 			throws XMLStreamException {
 		if (nextStartElement()
-				&& streamReader.getLocalName().trim().equals(localName)
+				&& graphSR.getLocalName().trim().equals(localName)
 				&& getAttributeValue(attributeLocalName).trim().equals(
 						attributeValue)
-				&& streamReader.next() == XMLStreamReader.CHARACTERS)
-			return streamReader.getText();
+				&& graphSR.next() == XMLStreamReader.CHARACTERS)
+			return graphSR.getText();
 
 		return null;
 	}
 
 	private boolean nextStartElement() throws XMLStreamException {
-		if (streamReader.hasNext()
-				&& (streamReader.next() == XMLStreamReader.START_ELEMENT || nextStartElement()))
+		if (graphSR.hasNext()
+				&& (graphSR.next() == XMLStreamReader.START_ELEMENT || nextStartElement()))
 			return true;
 
 		return false;
