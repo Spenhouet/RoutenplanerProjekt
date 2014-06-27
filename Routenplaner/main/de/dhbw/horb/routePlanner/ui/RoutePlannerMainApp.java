0c24abfa0d0abb7d8bf578461aacad5f9c371b8a
cOUGaa <rk-0815@web.de>
2014-06-27 09:29:54 +0200

    JavaFX GUI hinzugefügt

---
+++ Routenplaner/main/de/dhbw/horb/routePlanner/ui/RoutePlannerMainApp.java
@@ -0,0 +1,83 @@
+package de.dhbw.horb.routePlanner.ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class RoutePlannerMainApp extends Application {
+
+	public Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    public RoutePlannerMainApp() {
+    }
+    
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Draw Line");
+
+        initRootLayout();
+
+        showMainWindow();
+	}
+	
+	/**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(RoutePlannerMainApp.class.getResource("view/RoutePlannerRoot.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setTitle("Routenplaner");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showMainWindow() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(RoutePlannerMainApp.class.getResource("view/RoutePlannerMain.fxml"));
+            AnchorPane MainWindow = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(MainWindow);
+            
+            RoutePlannerMainController controller = loader.getController();
+            controller.setRoutePlannerMainApp(this);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+    
+    public static void main(String[] args) {
+    	launch(args);
+    }
+
+}
