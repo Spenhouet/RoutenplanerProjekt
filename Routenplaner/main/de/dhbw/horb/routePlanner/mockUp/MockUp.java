95d4c31fa827129c9aa352e3b1c9f98c1502a67e
Spenhouet <s.penhouet@outlook.de>
2014-06-30 15:43:30 +0200

    comma text

--- Routenplaner/main/de/dhbw/horb/routePlanner/mockUp/MockUp.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/mockUp/MockUp.java
@@ -1,16 +1,22 @@
 package de.dhbw.horb.routePlanner.mockUp;
 
 
-import de.dhbw.horb.routePlanner.parser.JDomGraphDataCreator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MockUp {
 
 	public static void main(String[] args) {
 
+		List<String> s = new ArrayList<String>();
+		s.add("test");
+		s.add("dsdf");
+		System.out.println(Arrays.toString(s.toArray()));
 		
-		JDomGraphDataCreator dom = new JDomGraphDataCreator();
-		
-		dom.createNodeXML();
+//		JDomGraphDataCreator dom = new JDomGraphDataCreator();
+//		
+//		dom.createNodeXML();
 		
 		
 //		GraphDataParser.getGraphDataParser(Constants.XML_GRAPHDATA).test();
