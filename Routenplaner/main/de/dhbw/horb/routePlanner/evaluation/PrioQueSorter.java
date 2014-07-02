97eb7d13200d9daf93d9ae7902d40ef5d46a174d
simon1994 <simon.stehle1994@gmail.com>
2014-07-02 11:53:31 +0200

    Klasse PrioQueSorter hinzugefügt

---
+++ Routenplaner/main/de/dhbw/horb/routePlanner/evaluation/PrioQueSorter.java
@@ -0,0 +1,27 @@
+package de.dhbw.horb.routePlanner.evaluation;
+
+import java.util.LinkedList;
+
+/**
+ * Klasse PrioQueSorter
+ * Sortiert die LinkedList neu
+ * Programmiert nach dem Insertionsort-Algorithmus
+ * @author Simon
+ *
+ */
+public class PrioQueSorter {
+
+	//TODO: nodePrice und prioQue Format!!!
+	public static LinkedList<Integer> sortNodes(int[] nodePrice, LinkedList<Integer> prioQue) {
+		for (int i = 0; i < prioQue.size(); i++) {
+			int temp = prioQue.get(i);
+			int j;
+			for (j = i - 1; j >= 0 && nodePrice[temp] < nodePrice[prioQue.get(j)]; j--) {
+				prioQue.set(j + 1, prioQue.get(j));
+			}
+			prioQue.set(j + 1, temp);
+		}
+
+		return prioQue;
+	}
+}
