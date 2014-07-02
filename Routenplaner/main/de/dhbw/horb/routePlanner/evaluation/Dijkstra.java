f46023f52f0ad1e35f98b5097034c0e318238c90
Simon <Simon@192.168.16.134>
2014-07-02 10:11:09 +0200

    Erste Logik hinzugefügt

--- Routenplaner/main/de/dhbw/horb/routePlanner/evaluation/Dijkstra.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/evaluation/Dijkstra.java
@@ -1,5 +1,30 @@
 package de.dhbw.horb.routePlanner.evaluation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Klasse Dijkstra
+ * Berechnet kürzeste Route von Start zu  Zielknoten
+ * @author Simon
+ *
+ */
 public class Dijkstra {
 
+	private Crossroads startnode;
+	private Crossroads endnode;
+	private String nearestNode;
+	private String[] nodePrice;
+	private boolean targetReached;
+	private List<Crossroads> prioQue = new ArrayList<Crossroads>();
+	private List<Crossroads> goneNodes = new ArrayList<Crossroads>();
+	private List<Crossroads> cheapNeighbours = new ArrayList<Crossroads>();
+	
+	
+	public Dijkstra(List<String> startnode, List<String> endnode){
+		this.startnode = new Crossroads(startnode);
+		this.endnode = new Crossroads(endnode);
+		
+	}
+	
 }
