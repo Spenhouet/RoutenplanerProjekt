e90be91b6bf68eb9442e25a88d796ba32ddfb2e3
Spenhouet <s.penhouet@outlook.de>
2014-07-05 13:05:02 +0200

    Neue XML Dateien immer erzeugen.

--- Routenplaner/main/de/dhbw/horb/routePlanner/mockUp/MockUp.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/mockUp/MockUp.java
@@ -1,25 +1,31 @@
 package de.dhbw.horb.routePlanner.mockUp;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import de.dhbw.horb.routePlanner.parser.JDomGraphDataCreator;
 
 public class MockUp {
 
 	public static void main(String[] args) {
-		
-		
+
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
+				DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:ms");
+				Calendar cal = Calendar.getInstance();
+				System.out.println(dateFormat.format(cal.getTime()));
+
 				JDomGraphDataCreator dom = new JDomGraphDataCreator();
 				dom.createNewXMLFiles();
-				
-				
+
+				cal = Calendar.getInstance();
+				System.out.println(dateFormat.format(cal.getTime()));
+
 			}
 		}).start();
-		
-		
-	}
 
+	}
 }
