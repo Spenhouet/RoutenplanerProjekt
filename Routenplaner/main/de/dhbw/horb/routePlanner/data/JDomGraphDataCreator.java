8daafe586e315b8943b368ebcd51fe9dea3a8034
Spenhouet <s.penhouet@outlook.de>
2014-08-14 18:36:05 +0200

    Einstellungen wieder geändert

--- Routenplaner/main/de/dhbw/horb/routePlanner/data/JDomGraphDataCreator.java
+++ Routenplaner/main/de/dhbw/horb/routePlanner/data/JDomGraphDataCreator.java
@@ -44,15 +44,14 @@ public class JDomGraphDataCreator {
 	    String highway = nodeMap.get(Constants.NODE_HIGHWAY);
 	    String name = nodeMap.get(Constants.NODE_NAME);
 
-	    if (name != null && highway != null && highway.equals(Constants.NODE_MOTORWAY_JUNCTION)) {
-		if (nm.containsKey(name)) {
+	    if (name != null && highway != null && highway.equals(Constants.NODE_MOTORWAY_JUNCTION))
+		if (nm.containsKey(name))
 		    nm.get(name).add(nodeID);
-		} else {
+		else {
 		    List<String> listIDs = new ArrayList<String>();
 		    listIDs.add(nodeID);
 		    nm.put(name, listIDs);
 		}
-	    }
 	}
 
 	for (Map.Entry<String, List<String>> entry : nm.entrySet()) {
@@ -82,26 +81,22 @@ public class JDomGraphDataCreator {
 	buildUpCache();
 
 	Map<String, List<String>> nodesXML = StAXMapGraphDataParser.getNodeXMLMap();
-	for (Map.Entry<String, List<String>> entry : nodesXML.entrySet()) {
+	for (Map.Entry<String, List<String>> entry : nodesXML.entrySet())
 	    for (String nodeID : entry.getValue()) {
-		if (nodeID == null || !SupportMethods.isNumeric(nodeID))
-		    continue;
+		if (nodeID == null || !SupportMethods.isNumeric(nodeID)) continue;
 		List<String> waysCont = getWaysContainingID(nodeID);
-		if (waysCont == null || waysCont.isEmpty())
-		    continue;
+		if (waysCont == null || waysCont.isEmpty()) continue;
 		for (String wayID : waysCont) {
 		    List<HashMap<String, String>> route = new ArrayList<HashMap<String, String>>();
 		    HashSet<String> idHistory = new HashSet<String>();
 		    HashMap<String, String> allInfos = getAllInfos(nodeID, wayID);
-		    if (allInfos == null)
-			continue;
+		    if (allInfos == null) continue;
 
 		    route.add(allInfos);
 		    idHistory.add(nodeID);
 		    recursRoute(route, idHistory);
 		}
 	    }
-	}
 
 	outp = new XMLOutputter();
 	outp.setFormat(Format.getPrettyFormat());
@@ -120,17 +115,15 @@ public class JDomGraphDataCreator {
 	    String wayID = entry.getKey();
 	    Map<String, String> wayInfos = entry.getValue();
 
-	    if (entry == null || wayID == null || wayInfos == null)
-		continue;
+	    if (entry == null || wayID == null || wayInfos == null) continue;
 
 	    List<String> nds = SupportMethods.commaStrToStrList(wayInfos.get(Constants.WAY_NODE));
 	    for (String nodeID : nds) {
-		if (nodeID == null || (nds.indexOf(nodeID) == (nds.size() - 1)))
-		    continue;
+		if (nodeID == null || nds.indexOf(nodeID) == nds.size() - 1) continue;
 
-		if (nodeWaysLink.containsKey(nodeID)) {
+		if (nodeWaysLink.containsKey(nodeID))
 		    nodeWaysLink.get(nodeID).add(wayID);
-		} else {
+		else {
 		    List<String> listIDs = new ArrayList<String>();
 		    listIDs.add(wayID);
 		    nodeWaysLink.put(nodeID, listIDs);
@@ -140,18 +133,16 @@ public class JDomGraphDataCreator {
     }
 
     private static void recursRoute(List<HashMap<String, String>> route, HashSet<String> idHistory)
-	    throws FileNotFoundException, IOException {
-	if (route == null || idHistory == null)
-	    return;
+												   throws FileNotFoundException,
+												   IOException {
+	if (route == null || idHistory == null) return;
 
 	HashMap<String, String> nextNode = getNextNode(route.get(route.size() - 1));
-	if (nextNode == null || nextNode.isEmpty())
-	    return;
+	if (nextNode == null || nextNode.isEmpty()) return;
 
 	String nextNodeID = nextNode.get(Constants.NODE_ID_EX);
 
-	if (idHistory.contains(nextNodeID))
-	    return;
+	if (idHistory.contains(nextNodeID)) return;
 
 	if (Constants.NODE_MOTORWAY_JUNCTION.equals(nextNode.get(Constants.NODE_HIGHWAY_EX))) {
 	    String departureNodeID = route.get(0).get(Constants.NODE_ID_EX);
@@ -168,13 +159,10 @@ public class JDomGraphDataCreator {
 
 	List<String> waysContain = getWaysContainingID(nextNodeID);
 
-	if (waysContain == null || waysContain.isEmpty()) {
-	    return;
-	}
+	if (waysContain == null || waysContain.isEmpty()) return;
 	for (String wayID : waysContain) {
 	    HashMap<String, String> nextNodeAllInfos = getAllInfos(nextNodeID, wayID);
-	    if (nextNodeAllInfos == null)
-		continue;
+	    if (nextNodeAllInfos == null) continue;
 	    route.add(nextNodeAllInfos);
 	    idHistory.add(nextNodeID);
 	    recursRoute(route, idHistory);
@@ -192,13 +180,11 @@ public class JDomGraphDataCreator {
     }
 
     private static HashMap<String, String> getAllInfos(String nodeID, String wayID) {
-	if (nodeID == null || wayID == null || nodeID.isEmpty() || wayID.isEmpty())
-	    return null;
+	if (nodeID == null || wayID == null || nodeID.isEmpty() || wayID.isEmpty()) return null;
 	Map<String, String> nodeInf = nodes.get(nodeID);
 	Map<String, String> wayInf = ways.get(wayID);
 
-	if (nodeInf == null || wayInf == null || nodeInf.isEmpty() || wayInf.isEmpty())
-	    return null;
+	if (nodeInf == null || wayInf == null || nodeInf.isEmpty() || wayInf.isEmpty()) return null;
 
 	HashMap<String, String> allInfos = new HashMap<String, String>();
 	allInfos.putAll(nodeInf);
@@ -212,22 +198,19 @@ public class JDomGraphDataCreator {
     }
 
     private static List<String> getWaysContainingID(String nodeID) {
-	if (nodeID == null)
-	    return null;
+	if (nodeID == null) return null;
 	return nodeWaysLink.get(nodeID);
     }
 
     private static void saveRoute(List<HashMap<String, String>> route) throws FileNotFoundException, IOException {
-	if (route == null || route.isEmpty())
-	    return;
+	if (route == null || route.isEmpty()) return;
 
 	String departureNodeName = route.get(0).get(Constants.NODE_NAME);
 	String departureNodeID = route.get(0).get(Constants.NODE_ID_EX);
 	String destinationNodeName = route.get(route.size() - 1).get(Constants.NODE_NAME);
 	String destinationNodeID = route.get(route.size() - 1).get(Constants.NODE_ID_EX);
 	if (departureNodeName == null || departureNodeID == null || destinationNodeName == null
-		|| destinationNodeID == null)
-	    return;
+		|| destinationNodeID == null) return;
 	Double distance = 0.0;
 	Long duration = 0L;
 	String ref = null;
@@ -235,8 +218,7 @@ public class JDomGraphDataCreator {
 
 	Map<String, String> lastNode = null;
 	for (Map<String, String> node : route) {
-	    if (node == null)
-		continue;
+	    if (node == null) continue;
 
 	    if (lastNode != null) {
 		Double lat1 = Double.valueOf(lastNode.get(Constants.NODE_LATITUDE));
@@ -244,31 +226,24 @@ public class JDomGraphDataCreator {
 		Double lat2 = Double.valueOf(node.get(Constants.NODE_LATITUDE));
 		Double lon2 = Double.valueOf(node.get(Constants.NODE_LONGITUDE));
 
-		if (lat1 == null || lon1 == null || lat2 == null || lon2 == null)
-		    continue;
+		if (lat1 == null || lon1 == null || lat2 == null || lon2 == null) continue;
 		Double dist = SupportMethods.fromLatLonToDistanceInKM(lat1, lon1, lat2, lon2);
-		if (dist == null || dist == 0.0)
-		    continue;
+		if (dist == null || dist == 0.0) continue;
 		distance += dist;
 		Integer speed = Integer.valueOf(lastNode.get(Constants.WAY_MAXSPEED));
-		if (speed == null || speed == 0)
-		    continue;
+		if (speed == null || speed == 0) continue;
 		Long dur = SupportMethods.fromDistanceAndSpeedToMilliseconds(dist, speed);
-		if (dur == null || dur == 0L)
-		    continue;
+		if (dur == null || dur == 0L) continue;
 		duration += dur;
 		if (ref == null) {
 		    String r = lastNode.get(Constants.WAY_REF);
-		    if (r != null && !r.isEmpty())
-			ref = r;
+		    if (r != null && !r.isEmpty()) ref = r;
 		}
 		String wayID = lastNode.get(Constants.WAY_ID_EX);
-		if (wayID != null && !wayIDs.contains(wayID))
-		    wayIDs.add(wayID);
+		if (wayID != null && !wayIDs.contains(wayID)) wayIDs.add(wayID);
 		lastNode = node;
-	    } else {
+	    } else
 		lastNode = node;
-	    }
 	}
 
 	Element newRoute = new Element(Constants.NEW_ROUTE);
@@ -276,8 +251,7 @@ public class JDomGraphDataCreator {
 	newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_DEPARTURENODENAME, departureNodeName));
 	newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_DESTINATIONNODEID, destinationNodeID));
 	newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_DESTINATIONNODENAME, destinationNodeName));
-	if (ref != null)
-	    newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_NUMBER, ref));
+	if (ref != null) newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_NUMBER, ref));
 	newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_DISTANCE, distance.toString()));
 	newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_DURATION, duration.toString()));
 	newRoute.setAttribute(new Attribute(Constants.NEW_ROUTE_WAYIDS, SupportMethods.strListToCommaStr(wayIDs)));
